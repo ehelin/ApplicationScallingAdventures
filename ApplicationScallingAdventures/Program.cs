bdffc68bb78814c8640cc8927a4b1ee97e26d88e
Eric Helin <ehelin@felpsis.net>
2016-05-17 10:37:11 -0500

    added to do

--- ApplicationScallingAdventures/Program.cs
+++ ApplicationScallingAdventures/Program.cs
@@ -10,6 +10,9 @@ namespace ApplicationScallingAdventures
             Run();
         }
 
+        //TODO - write a version of your scaling app to implement crdt's
+        //first write to a static list from multiple threads and have the results come out as expected
+        //http://www.se-radio.net/2016/03/se-radio-episode-252-christopher-meiklejohn-on-crdts/
         private static void Run()
         {
             Scaling scaling = new Scaling();
